eb1aa389ca2deb08d7054ab4bbe308d3bd61ad60
Aamir-shakil <aamirshakil21@gmail.com>
2024-11-27 12:03:47 +0000

    feat: add merge sort function

--- merge_sort.py
+++ merge_sort.py
@@ -1,3 +1,12 @@
 def merge_sort(arr):
     if len(arr) <= 1:
         return arr #Base case for recursion
+    
+    mid = len(arr)//2 #finds middle value
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:]) #uisng recursion to sort the left and right sight of the list from the middle value 
+    
+    return merge(left, right)
+
+def merge(arr):
+    
