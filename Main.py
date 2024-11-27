44415108c9f24b78d894a5b35e6d3f09f48ed012
Aamir-shakil <aamirshakil21@gmail.com>
2024-11-27 12:26:43 +0000

    feat: recalling merge_sort function in Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,8 @@
 # Creating a large data set from random number 
 import random
+import merge_sort
 large_dataset = [random.randint(0, 1000000) for _ in range(100000)] #generates 100,000 numbers
-print(large_dataset)
+# print(large_dataset)
+
+sorted_dataset = merge_sort.merge_sort(large_dataset)
+print(sorted_dataset)
